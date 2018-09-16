af5da0261f293ab284fdc927d718ea390b3b4100
youfang <981545521@qq.com>
2018-09-16 16:44:00 +0800

    init : import
    init : import

---
+++ src/main/java/cn/acyou/ibloges/IblogEsApplication.java
@@ -0,0 +1,12 @@
+package cn.acyou.ibloges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IblogEsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IblogEsApplication.class, args);
+	}
+}
