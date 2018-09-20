c942ffaf02a60df9b630cf271ed72af6a4e88e72
youfang <981545521@qq.com>
2018-09-20 19:37:14 +0800

    add : use guide

---
+++ notes/ElasticSearch.md
@@ -0,0 +1,11 @@
+### 使用指南
+- 官方下载：elasticsearch
+https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.6.11.zip
+- 官方下载：kibana
+https://artifacts.elastic.co/downloads/kibana/kibana-5.6.11-windows-x86.zip
+- 官方下载：logstash
+https://artifacts.elastic.co/downloads/logstash/logstash-5.6.11.zip
+
+
+### 配置文件：conf/elasticsearch.yml
+
--- src/test/java/cn/acyou/ibloges/demo/ElasticsearchTest1.java
+++ src/test/java/cn/acyou/ibloges/demo/ElasticsearchTest1.java
@@ -1,10 +1,15 @@
 package cn.acyou.ibloges.demo;
 
+import com.alibaba.fastjson.JSONObject;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.junit.After;
 import org.junit.Before;
@@ -16,6 +21,8 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author youfang
@@ -48,6 +55,7 @@ public class ElasticsearchTest1 {
     }
 
     /**
+     *  3、Elasticsearch索引库的创建
      * 创建索引库
      * 需求:创建一个索引库为：msg消息队列,类型为：tweet,id为1
      * 索引库的名称必须为小写
@@ -66,17 +74,93 @@ public class ElasticsearchTest1 {
 
 
     /**
-     * 测试Elasticsearch客户端连接
-     * @Title: test1
-     * @author sunt
-     * @date 2017年11月22日
-     * @return void
-     * @throws UnknownHostException
+     * 4、向索引库中添加json字符串
      */
-    @SuppressWarnings("resource")
     @Test
-    public void test1() throws UnknownHostException {
+    public void addIndex2() {
+        String jsonStr = "{" +
+                "\"userName\":\"张三\"," +
+                "\"sendDate\":\"2017-11-30\"," +
+                "\"msg\":\"你好李四\"" +
+                "}";
+        IndexResponse response = client.prepareIndex("weixin", "tweet").setSource(jsonStr, XContentType.JSON).get();
+        logger.info("json索引名称:" + response.getIndex() + "\njson类型:" + response.getType()
+                + "\njson文档ID:" + response.getId() + "\n当前实例json状态:" + response.status());
 
+    }
+
+
+    /**
+     * 5、向索引库添加一个Map集合
+     */
+    @Test
+    public void addIndex3() {
+        Map<String, Object> map = new HashMap<String,Object>();
+        map.put("userName", "张三");
+        map.put("sendDate", new Date());
+        map.put("msg", "你好李四");
+
+        IndexResponse response = client.prepareIndex("momo", "tweet").setSource(map).get();
+
+        logger.info("map索引名称:" + response.getIndex() + "\n map类型:" + response.getType()
+                + "\n map文档ID:" + response.getId() + "\n当前实例map状态:" + response.status());
+    }
+    /**
+     * 6、向索引库添加JsonObject
+     * 传递json对象
+     */
+    @Test
+    public void addIndex4() {
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("userName", "张三");
+        jsonObject.put("sendDate", "2017-11-23");
+        jsonObject.put("msg","你好李四");
+
+        IndexResponse response = client.prepareIndex("qq", "tweet").setSource(jsonObject, XContentType.JSON).get();
+
+        logger.info("jsonObject索引名称:" + response.getIndex() + "\n jsonObject类型:" + response.getType()
+                + "\n jsonObject文档ID:" + response.getId() + "\n当前实例jsonObject状态:" + response.status());
+    }
+
+
+    /**
+     * 7、从索引库获取数据
+     */
+    @Test
+    public void getData1() {
+        GetResponse getResponse = client.prepareGet("msg", "tweet", "1").get();
+        logger.info("索引库的数据:" + getResponse.getSourceAsString());
+    }
+
+    /**
+     * 8、更新索引库数据
+     */
+    @Test
+    public void updateData() {
+        JSONObject jsonObject = new JSONObject();
+
+        jsonObject.put("userName", "王五");
+        jsonObject.put("sendDate", "2008-08-08");
+        jsonObject.put("msg","你好,张三，好久不见");
+
+        UpdateResponse updateResponse = client.prepareUpdate("msg", "tweet", "1")
+                .setDoc(jsonObject.toString(),XContentType.JSON).get();
+
+        logger.info("updateResponse索引名称:" + updateResponse.getIndex() + "\n updateResponse类型:" + updateResponse.getType()
+                + "\n updateResponse文档ID:" + updateResponse.getId() + "\n当前实例updateResponse状态:" + updateResponse.status());
+    }
+
+    /**
+     * 9、删除索引库的数据
+     * 根据索引名称，类别，文档ID 删除索引库的数据
+     */
+    @Test
+    public void deleteData() {
+        DeleteResponse deleteResponse = client.prepareDelete("msg", "tweet", "1").get();
+
+        logger.info("deleteResponse索引名称:" + deleteResponse.getIndex() + "\n deleteResponse类型:" + deleteResponse.getType()
+                + "\n deleteResponse文档ID:" + deleteResponse.getId() + "\n当前实例deleteResponse状态:" + deleteResponse.status());
     }
 
 }
